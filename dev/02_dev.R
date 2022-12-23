9b47332fe8ec4ca05431aaf59e13147a373fb891
Alexandre Jaloto <alexandrejaloto@gmail.com>
2022-12-23 09:31:44 -0300

    bug no df_CH 4

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -36,7 +36,7 @@ golem::add_css_file( "custom" )
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw( name = "my_dataset", open = FALSE )
+usethis::use_data_raw( name = "df_CH", open = TRUE )
 
 ## Tests ----
 ## Add one line by test you want to create
